18559d255525c2e29d43af9556396098faeefc80
amelso <anthonymelsoner@gmail.com>
2021-08-25 20:11:56 -0400

    Post list view

---
+++ blog/src/components/Message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Message } from 'semantic-ui-react'
+
+export default ({ message, info, postive, warning, negative }) => (
+  <Message info postive warning negative>
+    {message}
+  </Message>
+)
+
+
--- blog/src/containers/PostList.js
+++ blog/src/containers/PostList.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import {Header} from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Divider, Image, Item, Header} from 'semantic-ui-react'
+import Loader from '../components/Loader';
+import Message from '../components/Message'
 
 const PostList = () => {
+  const [posts, setPosts] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const res = await axios.get('http://localhost:8000/posts/');
+        setPosts(res.data)
+        setLoading(false)
+      } catch (error) {
+        setError(error.message)
+        setLoading(false)
+      }
+
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div>
       <Header>Post List</Header>
+      <Divider/>
+      {error && <Message negative message={error} />}
+      {loading && <Loader />}
+      <Item.Group>
+        {posts?.map(post=> {
+          return (
+            <Item key={post.id}>
+              <Item.Image size='small' src={post.thumbnail} />
+              <Item.Content>
+                <Item.Header as='a'>{post.title}</Item.Header>
+                <Item.Description>{post.Content}</Item.Description>
+              </Item.Content>
+            </Item>
+          )
+        })}
+      </Item.Group>
     </div>
   )
 }
